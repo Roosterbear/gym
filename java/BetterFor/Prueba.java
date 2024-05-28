a8cc780e3983f6d4f75364691bd315ba4e555861
Roosterbear <lfpereag@gmail.com>
2024-05-27 23:28:54 -0600

    more code in py and java

---
+++ java/BetterFor/Prueba.java
@@ -0,0 +1,8 @@
+class Prueba{
+  public static void main(String[] args){
+    String[] nombres = {"Daniel", "Jeremias", "Panchito"};
+    for(String n: nombres){
+      System.out.println(n);
+    }
+  }
+}
---
+++ python/diccionario/prueba.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+fz = {'Frank Zappa': 'Cheap Thrills'}
+bs = {'Belle and Sebastian': 'Tiger Milk'}
+
+albums = {**fz,**bs}
+print(albums)
