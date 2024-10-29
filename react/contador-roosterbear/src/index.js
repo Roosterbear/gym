fbbb943431227f80d1e8809f7f71d2dee033b1f3
Roosterbear <lfpereag@gmail.com>
2024-10-29 00:25:17 -0600

    trabajando de react 17 a react 18

--- react/contador-roosterbear/src/index.js
+++ react/contador-roosterbear/src/index.js
@@ -1,17 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+
+const Fruta = (props)=>{
+  return (
+    <>
+      <h1>{props.name}</h1>
+      <hr/>
+    </>
+  )
+}
+
+const App = ()=>{
+  return (
+  <div>
+    <Fruta name={"Higo"} />
+    <Fruta name={"Mamey"} />
+    <Fruta name={"Kiwi"} />
+  </div>)
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <App/>
+)
